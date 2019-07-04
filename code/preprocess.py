237b60da58dbb3036c401c3f18a6faa6d459e387
Ibis Prevedello <ibiscp@gmail.com>
2019-07-04 15:42:17 +0200

    feat: remove smooth, generate 512x512 images and separate train and test

--- code/preprocess.py
+++ code/preprocess.py
@@ -3,44 +3,61 @@ import yaml
 import cv2
 import numpy as np
 from argparse import ArgumentParser
-import tensorflow as tf
-import sys
 import glob
+import math
+import shutil
+import random
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("dataset_path", nargs='?', default='../dataset/Bonn 2016 Median/CKA_160517/', help="Dataset path")
-    parser.add_argument("output_path", nargs='?', default='../dataset/Smoother/', help="Output path")
+    parser.add_argument("dataset_path", nargs='?', default='../dataset/Bonn 2016/', help="Dataset path")
+    parser.add_argument("output_path", nargs='?', default='../dataset/Processed/', help="Output path")
 
     return parser.parse_args()
 
+# Flip image
 def flip_image(img, mode = 1):
     # Horizontal = 1
     # Vertical = 0
     # Both = -1
     return cv2.flip(img, mode)
 
-def rotate_image(img, angle):
-    rows,cols = img.shape
-    M = cv2.getRotationMatrix2D((cols/2,rows/2), angle, 1)
-    return cv2.warpAffine(img,M,(cols,rows), borderMode=1)
+# Rotate image
+def rotate_image(img, angle, dim):
+    M = cv2.getRotationMatrix2D((dim/2,dim/2), angle, 1)
+    return cv2.warpAffine(img,M,(dim,dim), borderMode=1)
 
-def augment_image(img):
+# Return list of augmented images given one single image
+def augment_image(img, dim):
     flip_list = [-1, 0, 1]
-    rotation_list = [0, 90, 180, 270]
+    rotation_list = list(range(0, 360, 30))
     images = []
 
     for flip in flip_list:
         flip_img = flip_image(img, flip)
 
         for rotation in rotation_list:
-            rotation_img = rotate_image(flip_img, rotation)
+            rotation_img = rotate_image(flip_img, rotation, dim)
 
             images.append(rotation_img)
 
     return images
 
-def generate_dataset(path, output_path, dim = (128, 128), smooth = True, save_images=False):
+# Calculate maximum radius in pixel given contour and center stem
+def find_max_radius(contours, stem_x, stem_y):
+    dist = 0
+    for c in contours:
+        for point in c:
+            point = point[0]
+            x = point[0] - stem_x
+            y = point[1] - stem_y
+            new_dist = math.ceil(math.sqrt(math.pow(x, 2) + math.pow(y, 2)))
+            if new_dist > dist:
+                dist = new_dist
+
+    return dist
+
+def generate_dataset(path, output_path, dim = 512, smooth = False, save_images=False):
 
     annotationsPath = 'annotations/YAML/'
     nirImagesPath = 'images/nir/'
@@ -48,197 +65,146 @@ def generate_dataset(path, output_path, dim = (128, 128), smooth = True, save_im
     maskNirPath = 'annotations/dlp/iMap/'
     maskRgbPath = 'annotations/dlp/color/'
 
+    # Get folders
+    folders = glob.glob(path + '/*/')
+    print('Number of folders:', len(folders))
+    radius_list = []
 
-    # Get files
-    files = os.listdir(path + annotationsPath)
-    number_files = len(files)
-    print('Number of files: ', number_files)
+    for i, folder in enumerate(folders):
+        # Get files
+        files = os.listdir(folder + annotationsPath)
+        number_files = len(files)
+        print('\nFolder %d/%d: %s' %(i+1,len(folders),folder))
+        print('\tNumber of files:', number_files)
 
-    for file in files:
-        #file = 'bonirob_2016-05-17-11-42-26_14_frame196.yaml'
-        with open(path + annotationsPath + file, 'r') as stream:
-            # Image name
-            imageName = os.path.splitext(file)[0]
-            #print(imageName)
+        for j, file in enumerate(files):
+            # file = 'bonirob_2016-05-18-10-59-09_4_frame1.yaml'
+            print('\tFile %d/%d: %s' % (j + 1, len(files), file))
+            with open(folder + annotationsPath + file, 'r') as stream:
+                # Image name
+                imageName = os.path.splitext(file)[0]
 
-            # Open images
-            rgbimg = cv2.cvtColor(cv2.imread(path + rgbImagesPath + imageName + '.png', 1), cv2.COLOR_BGR2RGB)
-            nirimg = cv2.cvtColor(cv2.imread(path + nirImagesPath + imageName + '.png', 1), cv2.COLOR_BGR2RGB) #TODO convert to grayscale
-            maskNir = cv2.imread(path + maskNirPath + imageName + '.png', 1)
-            maskRgb = cv2.imread(path + maskRgbPath + imageName + '.png', 1)
-            # if rgbimg is None or nirimg is None or maskNir is None or maskRgb is None:
-            #     continue
-            imgrayRgb = maskRgb[:, :, 1]
-            imgrayNir = cv2.cvtColor(maskNir, cv2.COLOR_RGB2GRAY)
+                # Open images
+                rgbimg = cv2.imread(folder + rgbImagesPath + imageName + '.png', cv2.IMREAD_COLOR)
+                nirimg = cv2.imread(folder + nirImagesPath + imageName + '.png', cv2.IMREAD_GRAYSCALE)
+                maskNir = cv2.imread(folder + maskNirPath + imageName + '.png', cv2.IMREAD_GRAYSCALE)
+                maskRgb = cv2.imread(folder + maskRgbPath + imageName + '.png', cv2.IMREAD_COLOR) # Get only green channel
+                if rgbimg is None or nirimg is None or maskNir is None or maskRgb is None:
+                    continue
+                maskRgb = maskRgb[:, :,1]  # Get only green channel
 
-            # Image shape
-            shape = rgbimg.shape
+                # Image shape
+                shape = rgbimg.shape
 
-            # Get content from yaml file
-            content = yaml.safe_load(stream)
+                # Get content from yaml file
+                content = yaml.safe_load(stream)
 
-            # For each
-            for ann in content["annotation"]:
-                if ann['type'] == 'SugarBeets':
+                # For each
+                try:
+                    field = content["annotation"]
+                except:
+                    print('\t\tError: Empty Yaml')
+                    continue
+                for ann in field:
+                    if ann['type'] == 'SugarBeets':
 
-                    # Contours
-                    x = ann['contours'][0]['x']
-                    y = ann['contours'][0]['y']
+                        # Contours
+                        x = ann['contours'][0]['x']
+                        y = ann['contours'][0]['y']
 
-                    # Get stem
-                    stem_x = ann['stem']['x']
-                    stem_y = ann['stem']['y']
+                        # Get stem
+                        stem_x = ann['stem']['x']
+                        stem_y = ann['stem']['y']
 
-                    # Plant id
-                    id = ann['plant_id']
+                        # Plant id
+                        id = ann['plant_id']
 
-                    # Only consider if image is inside picture
-                    if (stem_y > 0 and stem_x > 0):
+                        # Only consider if image is inside picture
+                        if (stem_y > 0 and stem_x > 0):
 
-                        # Contour mask (roughy position of the plant)
-                        mask = np.zeros(shape=(rgbimg.shape[0], rgbimg.shape[1]), dtype="uint8")
-                        cv2.drawContours(mask, [np.array(list(zip(x, y)), dtype=np.int32)], -1, (255, 255, 255), -1)
+                            # Contour mask (roughy position of the plant)
+                            mask = np.zeros(shape=(rgbimg.shape[0], rgbimg.shape[1]), dtype="uint8")
+                            cv2.drawContours(mask, [np.array(list(zip(x, y)), dtype=np.int32)], -1, (255, 255, 255), -1)
 
-                        # Bitwise with RGB mask and most extreme points along the contour
-                        bitRgb = cv2.bitwise_and(imgrayRgb, imgrayRgb, mask=mask)
-                        _, contoursRgb, _ = cv2.findContours(bitRgb, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-                        if not contoursRgb:
-                            continue
-                        # cv2.drawContours(rgbimg, [np.array(list(zip(x, y)), dtype=np.int32)], 0, (0, 255, 0), 3)
-                        # cv2.imshow('image', rgbimg)
-                        # cv2.waitKey(0)
-                        # cv2.destroyAllWindows()
-                        c = max(contoursRgb, key=cv2.contourArea)
-                        leftRgb = tuple(c[c[:, :, 0].argmin()][0])[0]
-                        rightRgb = tuple(c[c[:, :, 0].argmax()][0])[0]
-                        topRgb = tuple(c[c[:, :, 1].argmin()][0])[1]
-                        botRgb = tuple(c[c[:, :, 1].argmax()][0])[1]
+                            # Bitwise with RGB mask and most extreme points along the contour
+                            bitRgb = cv2.bitwise_and(maskRgb, maskRgb, mask=mask)
+                            _, contoursRgb, _ = cv2.findContours(bitRgb, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+                            if not contoursRgb:
+                                continue
 
-                        # Bitwise with NIR mask and most extreme points along the contour
-                        bitNir = cv2.bitwise_and(imgrayNir, imgrayNir, mask=mask)
-                        _, contoursNir, _ = cv2.findContours(bitNir, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-                        c = max(contoursNir, key=cv2.contourArea)
-                        leftNir = tuple(c[c[:, :, 0].argmin()][0])[0]
-                        rightNir = tuple(c[c[:, :, 0].argmax()][0])[0]
-                        botNir = tuple(c[c[:, :, 1].argmin()][0])[1]
-                        topNir = tuple(c[c[:, :, 1].argmax()][0])[1]
+                            # Bitwise with NIR mask and most extreme points along the contour
+                            bitNir = cv2.bitwise_and(maskNir, maskNir, mask=mask)
+                            _, contoursNir, _ = cv2.findContours(bitNir, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
-                        # Final mask
-                        finalMask = np.zeros(shape=(shape[0], shape[1]), dtype="uint8")
-                        cv2.drawContours(finalMask, contoursRgb, -1, (255, 255, 255), -1)
-                        cv2.drawContours(finalMask, contoursNir, -1, (255, 255, 255), -1)
-                        if smooth:
-                            finalMask = cv2.blur(finalMask, (5,5))
+                            # Final mask
+                            finalMask = np.zeros(shape=(shape[0], shape[1]), dtype="uint8")
+                            cv2.drawContours(finalMask, contoursRgb, -1, (255, 255, 255), -1)
+                            cv2.drawContours(finalMask, contoursNir, -1, (255, 255, 255), -1)
+                            if smooth:
+                                finalMask = cv2.blur(finalMask, (5,5))
 
-                        # Final image
-                        finalRgb = cv2.bitwise_and(rgbimg, rgbimg, mask=finalMask)
-                        finalNir = cv2.bitwise_and(nirimg, nirimg, mask=finalMask)
+                            # Find maximum radius of the plant
+                            ret, thresh = cv2.threshold(finalMask, 127, 255, 0)
+                            im2, contours, hierarchy = cv2.findContours(thresh, 1, 2)
+                            radius = find_max_radius(contours, stem_x, stem_y)
+                            radius_list.append(radius)
+                            radius = int(radius * 1.1)
 
-                        maxHorizontal = max(rightRgb - stem_x, rightNir - stem_x, stem_x - leftRgb, stem_x - leftNir)
-                        maxVertical = max(topRgb - stem_y, topNir - stem_y, stem_y - botRgb, stem_y - botNir)
-                        maxTot = int(max(maxHorizontal, maxVertical) * 1.1)
-                        right = stem_x + maxTot
-                        left = stem_x - maxTot
-                        top = stem_y + maxTot
-                        bot = stem_y - maxTot
+                            # Final image
+                            finalRgb = cv2.bitwise_and(rgbimg, rgbimg, mask=finalMask)
+                            finalNir = cv2.bitwise_and(nirimg, nirimg, mask=finalMask)
 
-                        print(maxTot)
+                            right = stem_x + radius
+                            left = stem_x - radius
+                            top = stem_y + radius
+                            bot = stem_y - radius
 
-                        if bot > 0 and top < shape[0] and left > 0 and right < shape[1] and maxTot > 200:
-                            # Crop images
-                            cropRgb = finalRgb[bot:top, left:right, :]
-                            cropNir = finalNir[bot:top, left:right, :]
-                            cropMask = finalMask[bot:top, left:right]
+                            if bot > 0 and top < shape[0] and left > 0 and right < shape[1] and radius > dim/2:
+                                # Crop images
+                                cropRgb = finalRgb[bot:top, left:right, :]
+                                cropNir = finalNir[bot:top, left:right]
+                                cropMask = finalMask[bot:top, left:right]
 
-                            # backtorgb = cv2.cvtColor(bitNir, cv2.COLOR_GRAY2RGB)
-                            # cv2.rectangle(rgbimg, (right, bot), (left, top), (0, 255, 0), 1)
-                            # cv2.circle(rgbimg, (stem_x, stem_y), maxTot, (0, 255, 0), 1)
+                                # Resize image
+                                cropRgb = cv2.resize(cropRgb, (dim, dim), interpolation=cv2.INTER_AREA)
+                                cropNir = cv2.resize(cropNir, (dim, dim), interpolation=cv2.INTER_AREA)
+                                cropMask = cv2.resize(cropMask, (dim, dim), interpolation=cv2.INTER_AREA)
 
-                            # cv2.imshow('image', rgbimg)
-                            # cv2.waitKey(0)
-                            # cv2.destroyAllWindows()
+                                # Augment images
+                                cropRgb_ = augment_image(cropRgb, dim)
+                                cropNir_ = augment_image(cropNir, dim)
+                                cropMask_ = augment_image(cropMask, dim)
 
-                            # Resize image
-                            cropRgb = cv2.resize(cropRgb, dim, interpolation=cv2.INTER_AREA)
-                            cropNir = cv2.resize(cropNir, dim, interpolation=cv2.INTER_AREA)
-                            cropMask = cv2.resize(cropMask, dim, interpolation=cv2.INTER_AREA)
-
-                            cropMask_ = augment_image(cropMask)
-
-                            for i in range(len(cropMask_)):
-                                cv2.imwrite('../dataset/test/' + imageName + '_' + str(id) + '_' + str(i) + '.png', cropMask_[i])
-
-                            # # Write image
-                            # if save_images:
-                            #     #cv2.imwrite(output_path + 'RGB/' + imageName + '_' + str(id) + '.png', cropRgb)
-                            #     #cv2.imwrite(output_path + 'NIR/' + imageName + '_' + str(id) + '.png', cropNir)
-                            #     #cv2.imwrite(output_path + 'Mask/' + imageName + '_' + str(id) + '.png', cropMask)
-                            #     cv2.imwrite('../dataset/test/' + imageName + '_' + str(id) + '.png', cropMask)
-
-                            #yield cropRgb, cropNir, cropMask, imageName + '_' + str(id) + '.png'
-
-def image2tfrecords(img):
-    str = tf.compat.as_bytes(img.tostring())
-    bts = tf.train.BytesList(value=[str])
-    feature = tf.train.Feature(bytes_list=bts)
-
-    return feature
+                                # Write image
+                                if save_images:
+                                    for k in range(len(cropMask_)):
+                                        cv2.imwrite(output_path + 'train/rgb/' + imageName + '_' + str(id) + '_' + str(k) + '.png', cropRgb_[k])
+                                        cv2.imwrite(output_path + 'train/nir/' + imageName + '_' + str(id) + '_' + str(k) + '.png', cropNir_[k])
+                                        cv2.imwrite(output_path + 'train/mask/' + imageName + '_' + str(id) + '_' + str(k) + '.png', cropMask_[k])
 
 if __name__ == '__main__':
     args = parse_args()
 
-    # open the TFRecords file
-    data_path = '../dataset/train.tfrecords'  # address to save the TFRecords file
+    folders = ['train/', 'test/']
+    subfolers = ['mask/', 'rgb/', 'nir/']
+    # Create folders if do not exist
+    if os.path.exists(args.output_path):
+        print('\nFolder', args.output_path, 'already exist, delete it before continue!\n')
+    else:
+        print('\nCreating folders!\n')
 
-    # Load data
-    generate_dataset(path = args.dataset_path, output_path=args.output_path, save_images=True)
+        os.makedirs(args.output_path)
+        for f in folders:
+            for s in subfolers:
+                os.makedirs(args.output_path + f + s)
 
-    #writer = tf.python_io.TFRecordWriter(data_path)
+        # Generate data
+        generate_dataset(path=args.dataset_path, output_path=args.output_path, save_images=True)
 
-    # for i in generator:
-    #     rgb = i[0]
-    #     nir = i[1]
-    #     mask = i[2]
-    #     name = i[3]
-    #
-    #     feature = {name + '/rgb': image2tfrecords(rgb),
-    #                name + '/nir': image2tfrecords(nir),
-    #                name + '/mask': image2tfrecords(mask)}
-    #
-    #     # Create an example protocol buffer
-    #     example = tf.train.Example(features=tf.train.Features(feature=feature))
-    #
-    #     # Serialize to string and write on the file
-    #     writer.write(example.SerializeToString())
-    #
-    # writer.close()
-    # sys.stdout.flush()
+        # Split train and test files
+        files = os.listdir(args.output_path + folders[0] + 'mask/')
+        cut_files = random.sample(files, 100)
 
-
-
-
-    # with tf.Session() as sess:
-    #     feature = {'train/image': tf.FixedLenFeature([], tf.string),
-    #                'train/label': tf.FixedLenFeature([], tf.int64)}
-    #
-    #     # Create a list of filenames and pass it to a queue
-    #     filename_queue = tf.train.string_input_producer([data_path], num_epochs=1)
-    #
-    #     # Define a reader and read the next record
-    #     reader = tf.TFRecordReader()
-    #     _, serialized_example = reader.read(filename_queue)
-    #
-    #     # Decode the record read by the reader
-    #     features = tf.parse_single_example(serialized_example, features=feature)
-    #
-    #     # Convert the image data from string back to the numbers
-    #     image = tf.decode_raw(features['train/image'], tf.float32)
-    #
-    #     # Reshape image data into the original shape
-    #     image = tf.reshape(image, [224, 224, 3])
-    #
-    #     # Any preprocessing here ...
-    #
-    #     # Creates batches by randomly shuffling tensors
-    #     images, labels = tf.train.shuffle_batch([image, label], batch_size=10, capacity=30, num_threads=1,
-    #                                             min_after_dequeue=10)
+        for c in cut_files:
+            for s in subfolers:
+                shutil.move(args.output_path + folders[0] + s + c, args.output_path + folders[1] + s + c)
